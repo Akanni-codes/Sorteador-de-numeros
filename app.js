1f37f93415350131532effb691fbe8ffcf392fea
Akanni <akanni029@gmail.com>
2025-02-06 11:14:35 -0300

    gerando numeros aleatorios

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 function sortear() {
-    let quantidade = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
+    let quantidade = parentInt(document.getElementById('quantidade').value);
+    let de = parentInt(document.getElementById('de').value);
+    let ate = parentInt(document.getElementById('ate').value);
+
+    let sortudos [];
+    let numero;
+
+    for (let i = 0 < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+        sortudos.push(numero);
+    }
+}
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
